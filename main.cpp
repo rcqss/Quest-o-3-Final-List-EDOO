36a324ac9dcc1cb1e8b7ac69e9e61e56ff90d151
Rafael Sales <152890130+rcqss@users.noreply.github.com>
2025-04-04 16:20:41 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main() {
     const string inputFilename = "sequences.fasta.txt";
     const string outputFilename = "genome_analysis_output.txt";
+    //const string inputFilename = "sequences.rafael.txt";
+    //const string outputFilename = "genome_analysis_output_rafael.txt";
 
     try {
         processGenome(inputFilename, outputFilename);
